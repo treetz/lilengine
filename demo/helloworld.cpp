bcc2ae05047004b6e49afa2f328c296eed75866d
treetz <treetz@gmu.edu>
2022-09-02 00:19:41 -0400

    Checkpoint 2

--- demo/helloworld.cpp
+++ demo/helloworld.cpp
@@ -1,6 +1,14 @@
+#include "Engine.h"
+
 #include <iostream>
 
 int main(int argc, const char* argv[]) {
+	using namespace lilengine;
+
+	Engine e;
+	e.Startup();
+	e.RunGameLoop();
+
 	std::cout << "Hello, World!\n";
 	return 0;
 }
