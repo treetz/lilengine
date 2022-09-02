76f2acc317ffaec018bb10768b8b9ceb4af328b2
treetz <treetz@gmu.edu>
2022-09-02 00:20:43 -0400

    Checkpoint 2

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,26 @@
 add_rules("mode.debug", "mode.release")
+add_requires("glfw")
+
+set_policy("build.warning", true) -- show warnings
+set_warnings("all") -- warn about many things
 
 target("helloworld")
     set_kind("binary")
     set_languages("cxx17")
+
+	add_deps("lilengine")
     
     add_files("demo/helloworld.cpp")
+
+target("lilengine")
+    set_kind("static")
+    set_languages("cxx17")
+    
+    -- Declare our engine's header path.
+    -- This allows targets that depend on the engine to #include them.
+    add_includedirs("src", {public = true})
+    
+    -- Add all .cpp files in the `src` directory.
+    add_files("src/*.cpp")
+
+	add_packages("glfw")
