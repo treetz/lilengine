c90ed8b6e78c36cb4da57e0a23077375bba30a91
treetz <treetz@gmu.edu>
2022-09-13 23:09:19 -0400

    git Update

--- demo/helloworld.cpp
+++ demo/helloworld.cpp
@@ -2,12 +2,17 @@
 
 #include <iostream>
 
+void KeyIsDown(int key) {
+
+}
+
 int main(int argc, const char* argv[]) {
 	using namespace lilengine;
 
 	Engine e;
 	e.Startup();
-	e.RunGameLoop();
+	e.RunGameLoop(NULL);
+	e.Shutdown();
 
 	std::cout << "Hello, World!\n";
 	return 0;
