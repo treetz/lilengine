856ba23ad608d58b50e0b3d8bc2cddd02687fce1
treetz <treetz@gmu.edu>
2022-11-03 01:24:28 -0400

    Working on Pong Build

--- assets/scripts/pong_setup.lua
+++ assets/scripts/pong_setup.lua
@@ -1,28 +1,33 @@
+-- Load in all assets
 LoadSound("bubbles", "bubbles.wav")
 LoadImage("spongebob", "spongebob.png")
 
+-- Create the ball game object and set up its components
 ball = CreateGameObject()
 
 ball_sprite = GetSprite(ball)
 ball_sprite.image_name = "spongebob"
-ball_sprite.scale = vec3(100, 100, 100)
-ball_sprite.rotation_axis = vec3(0, 0, 0)
+ball_sprite.scale = vec3.new(10.0, 10.0, 10.0)
+ball_sprite.rotation_axis = vec3.new(0.0, 0.0, 0.0)
 ball_sprite.rotation_angle = 0
 ball_sprite.z = 1.0
 
--- Things that do not work:
+ball_position = GetPosition(ball)
+ball_position.x = 0.0
+ball_position.y = 0.0
 
---GetSprite(ball)= ball_sprite
---GetSprite(ball) = Sprite.new()
+ball_velocity = GetVelocity(ball)
+ball_velocity.x = 0.0
+ball_velocity.y = 0.0
 
---ball_sprite = GetSprite(ball)
---ball_sprite.image_name = "spongebob"
+ball_hitbox_right = 10.0
+ball_hitbox_left = 10.0
+ball_hitbox_top = 5.0
+ball_hitbox_bottom = 5.0
 
---GetSprite(ball).image_name = "spongebob"
---GetSprite(ball).scale = vec3.new(100.0, 100.0, 0.0)
---GetSprite(ball).rotation_axis = vec3.new(0, 0, 0)
---GetSprite(ball).rotation_angle = 0.0
---GetSprite(ball).z = 1.0
+-- Create the player game objects and set up their components
+player_1 = CreateGameObject()
 
---GetPosition(ball).x = 50.0
---GetPosition(ball).y = 0.0
+
+
+ball_status = "ready"
--- assets/scripts/pong_update.lua
+++ assets/scripts/pong_update.lua
@@ -1,1 +1,62 @@
-print("hello")
+-- GAME STATES
+-- If the ball is ready for the start of the put it at the center of 
+-- the screen
+if ball_status == "ready" then 
+	ball_position.x = 0.0
+	ball_position.y = 0.0
+	ball_velocity.x = 0.0
+	ball_velocity.y = 0.0
+end
+
+-- If it is the start of the game then give the ball a random initial
+-- velocity.
+if ball_status == "start" then
+	if math.random(0, 1) == 0 then
+		ball_velocity.x = 50.0
+		ball_velocity.y = math.random(-50.0, 50.0)
+	else
+		ball_velocity.x = -50.0
+		ball_velocity.y = math.random(-50.0, 50.0)
+	end
+	ball_status = "play"
+end
+
+-- KEYBOARD INPUT
+-- Start the game if the spacebar is pressed.
+if KeyIsDown(KEYBOARD.SPACE) and (ball_status == "ready") then
+	ball_status = "start"
+end
+
+-- Reset the ball if the R key is pressed.
+if KeyIsDown(KEYBOARD.R) and (ball_status == "play") then 
+	ball_status = "ready"
+end
+
+-- HITBOXES
+if (ball_position.x + ball_hitbox_right) > (GetWindowWidth() / 4.5) then
+	ball_velocity.x = ball_velocity.x * -1.1
+	PlaySound("bubbles")
+end
+
+if (ball_position.x - ball_hitbox_left) < ((GetWindowWidth() / 4.5) * -1) then
+	ball_velocity.x = ball_velocity.x * -1.1
+	PlaySound("bubbles")
+end
+
+if (ball_position.y + ball_hitbox_top) > (GetWindowHeight() / 4.5) then
+	ball_velocity.y = ball_velocity.y * -1.1
+	PlaySound("bubbles")
+end
+
+if (ball_position.y - ball_hitbox_bottom) < ((GetWindowHeight() / 4.5) * -1) then
+	ball_velocity.y = ball_velocity.y * -1.1
+	PlaySound("bubbles")
+end
+
+-- BALL MOVEMENT
+-- Move the ball according to its velocity.
+ball_position.x = ball_position.x + (ball_velocity.x / 60)
+ball_position.y = ball_position.y + (ball_velocity.y / 60)
+
+print(ball_position.x)
+print(ball_position.y)
