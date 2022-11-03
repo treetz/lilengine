b36c508eef306ea53dbdcc1386a764da9b40485b
treetz <treetz@gmu.edu>
2022-11-03 11:59:16 -0400

    Working on Pong Build

--- assets/scripts/pong_setup.lua
+++ assets/scripts/pong_setup.lua
@@ -1,16 +1,18 @@
 -- Load in all assets
 LoadSound("bubbles", "bubbles.wav")
 LoadImage("spongebob", "spongebob.png")
+--LoadImage("the_office", "the_office.png")
+LoadImage("paddle", "paddle.png")
 
 -- Create the ball game object and set up its components
 ball = CreateGameObject()
 
 ball_sprite = GetSprite(ball)
 ball_sprite.image_name = "spongebob"
-ball_sprite.scale = vec3.new(10.0, 10.0, 10.0)
+ball_sprite.scale = vec3.new(10.0, 10.0, 0.0)
 ball_sprite.rotation_axis = vec3.new(0.0, 0.0, 0.0)
 ball_sprite.rotation_angle = 0
-ball_sprite.z = 1.0
+ball_sprite.z = 0.0
 
 ball_position = GetPosition(ball)
 ball_position.x = 0.0
@@ -25,9 +27,51 @@ ball_hitbox_left = 10.0
 ball_hitbox_top = 5.0
 ball_hitbox_bottom = 5.0
 
--- Create the player game objects and set up their components
+-- Create the player game objects and set up their components.
+-- Player 1
 player_1 = CreateGameObject()
 
+player_1_sprite = GetSprite(player_1)
+player_1_sprite.image_name = "paddle"
+player_1_sprite.scale = vec3.new(15.0, 15.0, 0.0)
+player_1_sprite.rotation_axis = vec3.new(0.0, 0.0, 0.0)
+player_1_sprite.rotation_angle = 0
+player_1_sprite.z = 0.1
 
+player_1_position = GetPosition(player_1)
+player_1_position.x = 165.0
+player_1_position.y = 0.0
+
+player_1_velocity = GetVelocity(player_1)
+player_1_velocity.x = 0.0
+player_1_velocity.y = 0.0
+
+player_1_hitbox_right = 4.5
+player_1_hitbox_left = 4.5
+player_1_hitbox_top = 15.0
+bplayer_1_hitbox_bottom = 15.0
+
+-- Player 2
+player_2 = CreateGameObject()
+
+player_2_sprite = GetSprite(player_2)
+player_2_sprite.image_name = "paddle"
+player_2_sprite.scale = vec3.new(15.0, 15.0, 0.0)
+player_2_sprite.rotation_axis = vec3.new(0.0, 0.0, 0.0)
+player_2_sprite.rotation_angle = 0
+player_2_sprite.z = 0.1
+
+player_2_position = GetPosition(player_2)
+player_2_position.x = -165.0
+player_2_position.y = 0.0
+
+player_2_velocity = GetVelocity(player_2)
+player_2_velocity.x = 0.0
+player_2_velocity.y = 0.0
+
+player_2_hitbox_right = 4.25
+player_2_hitbox_left = 4.25
+player_2_hitbox_top = 15.0
+bplayer_2_hitbox_bottom = 15.0
 
 ball_status = "ready"
--- assets/scripts/pong_update.lua
+++ assets/scripts/pong_update.lua
@@ -21,7 +21,7 @@ if ball_status == "start" then
 	ball_status = "play"
 end
 
--- KEYBOARD INPUT
+-- PLAYER INPUT
 -- Start the game if the spacebar is pressed.
 if KeyIsDown(KEYBOARD.SPACE) and (ball_status == "ready") then
 	ball_status = "start"
@@ -32,7 +32,28 @@ if KeyIsDown(KEYBOARD.R) and (ball_status == "play") then
 	ball_status = "ready"
 end
 
+-- Move player 1 up and down based on if the user presses the up or
+-- down arrow key.
+if KeyIsDown(KEYBOARD.UP) then
+	player_1_velocity.y = 150
+elseif KeyIsDown(KEYBOARD.DOWN) then
+	player_1_velocity.y = -150
+else
+	player_1_velocity.y = 0
+end
+
+-- Move player 2 up and down based on if the user presses the W or S
+-- key.
+if KeyIsDown(KEYBOARD.W) then
+	player_2_velocity.y = 150
+elseif KeyIsDown(KEYBOARD.S) then
+	player_2_velocity.y = -150
+else
+	player_2_velocity.y = 0
+end
+
 -- HITBOXES
+-- Make the ball bounce off the four "walls" of the screen.
 if (ball_position.x + ball_hitbox_right) > (GetWindowWidth() / 4.5) then
 	ball_velocity.x = ball_velocity.x * -1.1
 	PlaySound("bubbles")
@@ -53,10 +74,26 @@ if (ball_position.y - ball_hitbox_bottom) < ((GetWindowHeight() / 4.5) * -1) the
 	PlaySound("bubbles")
 end
 
--- BALL MOVEMENT
+-- Make the ball bounce off the players.
+--if ((ball_position.x - ball_hitbox_left) < (player_2_position.x + player_2_hitbox_right)) then
+--	if (ball_position.y < player_2_position.y + player_2_hitbox_top) then
+--		if (ball_position.y > player_2_position.y - player_2_hitbox_bottom) then
+--			ball_velocity.x = ball_velocity.x * -1.1
+--			PlaySound("bubbles")
+--		end
+--	end
+--end
+
+
+-- Bound the players so they cannot move off the screen
+--if (player_1_position.y + player_1_hitbox_top) > (GetWindowHeight() / 4.5) then
+-- 	player_1_velocity.y = ball_velocity.y * -1.0
+--end
+
+-- BALL AND PLAYER MOVEMENT
 -- Move the ball according to its velocity.
 ball_position.x = ball_position.x + (ball_velocity.x / 60)
 ball_position.y = ball_position.y + (ball_velocity.y / 60)
 
-print(ball_position.x)
-print(ball_position.y)
+player_1_position.y = player_1_position.y + (player_1_velocity.y / 60)
+player_2_position.y = player_2_position.y + (player_2_velocity.y / 60)
